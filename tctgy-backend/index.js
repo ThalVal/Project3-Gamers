06e040745453a539a780c90656feb119d93dacd3
Kiri Kiri <killian.podhajsky@gmail.com>
2023-06-05 20:20:31 -0700

    Added to some models, compared for front end connections

---
+++ tctgy-backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const allRoutes = require('./controllers');
+const cors = require("cors")
+const sequelize = require('./config/connection');
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3001;
+// Requiring our models for syncing
+/* const { User,Pallet} = require('./models'); */
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/* app.use(cors()) */
+
+/* app.use('/',allRoutes); */
+app.get('/', (req,res)=>{
+    res.send('Lets gitit');
+})
+
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+    });
+});
---
+++ tctgy-backend/routes/api/userRoutes.js
@@ -0,0 +1,79 @@
+const router = require('express').Router();
+const User = require('../../models/User');
+const bcrypt = require('bcrypt');
+
+// GET all users
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll();
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// CREATE new user
+router.post('/', async (req, res) => {
+    try {
+        const newUser = req.body;
+        newUser.password = await bcrypt.hash(req.body.password, 10);
+        const userData = await User.create(newUser);
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET one user
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.user_id);
+        if (!userData) {
+            res.status(404).json({ message: 'No user with this ID :('});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// UPDATE a user
+router.put('/:id', async (req, res) => {
+    try {
+        const userData = await User.update(req.body, {
+            where: {
+                user_id: req.params.user_id,
+            },
+        });
+        if (!userData[0]) {
+            res.status(404).json({ message: 'No user with this ID :('});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//DELETE a user
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: {
+                user_id: req.params.user_id,
+            },
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user with this ID :('});
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
