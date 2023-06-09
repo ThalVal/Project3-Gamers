549baced79e9d707ad651f977c8bd7ccca3b3348
ChickenChamp71 <chickensandsharks@gmail.com>
2023-06-09 12:23:11 -0700

    changed formatting for link

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-    origin: ["https://theyrecomingtogetyou.netlify.app/"],
+    origin: ["https://theyrecomingtogetyou.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ['Content-Type'],
     maxAgeSeconds: 3600
